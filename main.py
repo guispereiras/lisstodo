859c88e2b2d8605927bbc7121d50f5a67ce5b2de
guispereiras <guisilva.pereira@hotmail.com>
2023-10-31 22:07:56 -0300

    login and sign in update

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
-
-
-
-@app.route("/")
+# Rota home
+@app.route("/") 
 def homepage():
-    return render_template("homepage.html")
+    return render_template("home.html")
 
+# Rota para ler o login.html antes de executa-lo (Gambiarrazap)
+@app.route("/login")
+def login1():
+    return render_template("login.html")
 
+# Rota cadastro
 @app.route("/cadastro")
 def atributos():
      return render_template("cadastro.html")
 
+# Rota ler login.html e puxar os dados dos input's
 @app.route("/login", methods=['POST'])
-def login():
+def login2():
     
-    nome = request.form.get("nome")
+    email = request.form.get("email")
     senha = request.form.get("senha")
-    print(nome)
+    print(email)
     print(senha)
 
-    return redirect("/")
+    return redirect("/cadastro")
 
 if __name__ == "__main__":
     app.run(debug=True)
