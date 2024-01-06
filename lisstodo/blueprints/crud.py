c3843536246e0fb8a49de965458c664ab4cbcff3
ViniciusTeleSS <vinitelessilva@hotmail.com>
2024-01-06 16:44:08 -0300

    Login verify

--- lisstodo/blueprints/crud.py
+++ lisstodo/blueprints/crud.py
@@ -4,13 +4,22 @@ from lisstodo.ext.connection import mydb
 
 def create(email, nome, idade, sexo, senha):
     cursor = mydb.cursor()
-
-    comando = f'INSERT INTO usuarios (ID, NOME, EMAIL, SEXO, IDADE, SENHA) VALUES (DEFAULT, "{nome}", "{email}", "{sexo}", "{idade}","{senha}")'
+    comando = f'SELECT email FROM usuarios where email = "{email}"'
     cursor.execute(comando)
-    mydb.commit()
-    print('Registro adicionado com sucesso')
-    cursor.close()
-    mydb.close()
+    resultado = cursor.fetchone()
+
+    if resultado:
+        email_banco = resultado[0]
+        if email == email_banco:
+            print("E-mail já cadastrado")
+            return("E-mail já cadastrado")
+        else:
+            comando = f'INSERT INTO usuarios (ID, NOME, EMAIL, SEXO, IDADE, SENHA) VALUES (DEFAULT, "{nome}", "{email}", "{sexo}", "{idade}","{senha}")'
+            cursor.execute(comando)
+            mydb.commit()
+            print('Registro adicionado com sucesso')
+            cursor.close()
+            mydb.close()
 
 def login(email, senha):
     cursor = mydb.cursor()
